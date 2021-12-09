f2e0608a8cf7d0f5e91e0caf1f6720958e84b1fd
ivan <805632731@qq.com>
2021-12-10 00:01:58 +0800

    fix ResponseAdvice

--- tamao-core-tool/src/main/java/com/ivan/tamo/api/ResponseAdvice.java
+++ tamao-core-tool/src/main/java/com/ivan/tamo/api/ResponseAdvice.java
@@ -1,16 +1,17 @@
 package com.ivan.tamo.api;
 
-import cn.hutool.core.util.StrUtil;
+
 import cn.hutool.json.JSONUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.ivan.tamo.api.annotation.CustomResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+import java.lang.reflect.Method;
 
 /**
  * <p>全局返回处理</p>
@@ -19,26 +20,54 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * @className ResponseAdvice
  * @since 2021/12/7 23:17
  */
+@Slf4j
 @RestControllerAdvice
 public class ResponseAdvice implements ResponseBodyAdvice<Object> {
 
+    /**
+     * 是否需要处理返回结果
+     *
+     * @param methodParameter
+     * @param aClass
+     * @return
+     */
     @Override
-    public boolean supports(MethodParameter returnType, Class converterType) {
-        return true;
+    public boolean supports(MethodParameter methodParameter, Class aClass) {
+
+        log.info("In supports() method of " + getClass().getSimpleName());
+
+        Method method = methodParameter.getMethod();
+
+        assert method != null;
+
+        return method.isAnnotationPresent(CustomResponse.class);
     }
 
+    /**
+     * 处理返回结果
+     *
+     * @param body
+     * @param methodParameter
+     * @param mediaType
+     * @param aClass
+     * @param serverHttpRequest
+     * @param serverHttpResponse
+     * @return
+     */
     @Override
-    @SneakyThrows
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if(body instanceof String){
-            return JSONUtil.toJsonStr(R.data(body));
-        }
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter methodParameter,
+                                  MediaType mediaType,
+                                  Class aClass,
+                                  ServerHttpRequest serverHttpRequest,
+                                  ServerHttpResponse serverHttpResponse) {
+
+        log.info("In beforeBodyWrite() method of " + getClass().getSimpleName());
 
         if (body instanceof R) {
             return body;
         }
 
-        return R.success(body.toString());
-
+        return R.data(body);
     }
 }
---
+++ tamao-core-tool/src/main/java/com/ivan/tamo/config/TamaoToolAutoConfiguration.java
@@ -0,0 +1,38 @@
+package com.ivan.tamo.config;
+
+import com.ivan.tamo.api.ResponseAdvice;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * <p></p>
+ *
+ * @author ivan
+ * @className TamaoToolAutoConfiguration
+ * @since 2021/12/9 23:45
+ */
+@Configuration(proxyBeanMethods = false)
+public class TamaoToolAutoConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public ResponseAdvice responseAdvice() {
+        return new ResponseAdvice();
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 解决 String 统一封装RestBody的问题
+        HttpMessageConverter<?> httpMessageConverter = converters.get(7);
+        if (!(httpMessageConverter instanceof MappingJackson2HttpMessageConverter)) {
+            // 确保正确，如果有改动就重新debug
+            throw new RuntimeException("MappingJackson2HttpMessageConverter is not here");
+        }
+        converters.add(0, httpMessageConverter);
+    }
+
+}
